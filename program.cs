f95d3353534e2cf061df00006e3d6cf225fde382
DAAN <daanmgneni@gmail.com>
2023-03-16 23:36:34 +0530

    Main program

---
+++ program.cs
@@ -0,0 +1,22 @@
+namespace Helloworld
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            // leapyear.year();
+            // basics.display();
+            //flipcoin.flipthecoin();
+            //power_2.call();
+            //harmonic.call();
+            //factorization.call();
+            //VowelAndConsonent.call();
+            //SwapTwoNumber.call();
+            //LargestNumber.call();
+            // QuotientAndRemainder.call();
+            EvenOdd.call();
+
+            
+        }
+    }
+}
